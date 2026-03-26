f7e45794bf0a58463dc3bc5fd126365dd6959819
AtlassianDell <cookloganp@gmail.com>
2026-03-26 16:02:10 -0400

    deleted everything just cuz

--- projects/vertigo/adstring/adstring.py
+++
@@ -1,7 +0,0 @@
-def handle_split(parts):
- result = list(get_value(parts[1]))
- result.reverse()
- for i in range(len(result)):
-  stacks[curstack].append(result[i])
-
-instruction_handlers["SPLIT"] = handle_split
